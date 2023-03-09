5aa104f152fd410056d2b6a4809c8549ca8bf8f4
kapibarra2315 <nikita.nov2016@yandex.ru>
2023-03-10 00:59:32 +0300

    new styles for loader

--- src/app/main-page/shared/components/loader/loader.component.ts
+++ src/app/main-page/shared/components/loader/loader.component.ts
@@ -6,7 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./loader.component.scss'],
 })
 export class LoaderComponent implements OnInit {
+  showLoader? = false;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.showFor5Seconds();
+  }
+  showFor5Seconds() {
+    this.showLoader = true;
+    let a = this;
+    setTimeout(() => {
+      this.showLoader = false;
+    }, Math.floor(Math.random() * 2000));
+  }
 }
