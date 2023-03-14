c0bbda3dbc1e7914bd06e857108e2f509f2f68c1
kapibarra2315 <nikita.nov2016@yandex.ru>
2023-03-14 14:33:10 +0300

    added firebase

--- src/app/main-page/shared/services/common.service.ts
+++ src/app/main-page/shared/services/common.service.ts
@@ -21,3 +21,20 @@ export class CommonService {
   //   };
   // }
 }
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCXQVteVX7_HYErkRtdK84tS9b5olHxfok',
+  authDomain: 'novikovstudio-4c512.firebaseapp.com',
+  projectId: 'novikovstudio-4c512',
+  storageBucket: 'novikovstudio-4c512.appspot.com',
+  messagingSenderId: '20614670915',
+  appId: '1:20614670915:web:d73dd029597b6499bfc2f6',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
