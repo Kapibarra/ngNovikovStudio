e8c1b59ffb50a436b7422d51cf05517b7989b8c4
kapibarra2315 <nikita.nov2016@yandex.ru>
2023-04-12 01:18:03 +0300

    added custom cursor

--- src/app/main-page/shared/components/cursor/cursor.component.ts
+++ src/app/main-page/shared/components/cursor/cursor.component.ts
@@ -5,13 +5,35 @@ import {
   ElementRef,
   ViewChild,
 } from '@angular/core';
+import { fromEvent, interval } from 'rxjs';
+import { throttle } from 'rxjs/operators';
 @Component({
   selector: 'app-cursor',
   templateUrl: './cursor.component.html',
   styleUrls: ['./cursor.component.scss'],
 })
 export class CursorComponent implements OnInit {
+  top: string = '0px';
+  left: string = '0px';
+  expand = false;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    const mouseMove$ = fromEvent(document, 'mousemove').pipe(
+      throttle(() => interval(16))
+    );
+    mouseMove$.subscribe((event: Event) => {
+      const mouseEvent = event as MouseEvent;
+      this.top = mouseEvent.pageY - 10 + 'px';
+      this.left = mouseEvent.pageX - 10 + 'px';
+    });
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClick() {
+    this.expand = true;
+    setTimeout(() => {
+      this.expand = false;
+    }, 500);
+  }
 }
