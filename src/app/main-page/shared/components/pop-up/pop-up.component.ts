44ac65f3fd4be641aac20f66abff665d50376d77
kapibarra2315 <nikita.nov2016@yandex.ru>
2023-04-12 15:21:54 +0300

    popup overlay

--- src/app/main-page/shared/components/pop-up/pop-up.component.ts
+++ src/app/main-page/shared/components/pop-up/pop-up.component.ts
@@ -1,10 +1,22 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { DialogService } from '../../services/dialog.service';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-pop-up',
   templateUrl: './pop-up.component.html',
   styleUrls: ['./pop-up.component.scss'],
+  animations: [
+    trigger('popupAnimation', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('300ms cubic-bezier(.35,0,.25,1)', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('300ms cubic-bezier(.35,0,.25,1)', style({ opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class PopUpComponent implements OnInit {
   isActive: boolean = false;
