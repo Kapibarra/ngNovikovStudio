4e0fe5657bb9eccd526b77b0d7a86342a490e1b5
kapibarra2315 <nikita.nov2016@yandex.ru>
2023-04-13 02:06:07 +0300

    contacts component

--- src/app/main-page/shared/components/primary-button-big/primary-button-big.component.ts
+++ src/app/main-page/shared/components/primary-button-big/primary-button-big.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class PrimaryButtonBigComponent implements OnInit {
   @Input() label!: string;
+  @Input() disabled!: boolean;
   constructor() {}
 
   ngOnInit(): void {}
