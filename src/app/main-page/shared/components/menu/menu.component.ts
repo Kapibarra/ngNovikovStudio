3044be42fce8ca531a4c60341ba9f3c62633f355
kapibarra2315 <nikita.nov2016@yandex.ru>
2023-04-12 18:04:11 +0300

    header and menu component

--- src/app/main-page/shared/components/menu/menu.component.ts
+++ src/app/main-page/shared/components/menu/menu.component.ts
@@ -1,15 +1,51 @@
 import { ViewportScroller } from '@angular/common';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 
+export const slideInOutAnimation = trigger('slideInOutAnimation', [
+  state(
+    'in',
+    style({
+      transform: 'translateY(0)',
+      opacity: 1,
+    })
+  ),
+  state(
+    'out',
+    style({
+      transform: 'translateY(-100%)',
+      opacity: 0,
+    })
+  ),
+  transition('in => out', animate('400ms ease-out')),
+  transition('out => in', animate('400ms ease-in')),
+]);
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
+  animations: [slideInOutAnimation],
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent implements OnInit, AfterViewInit {
   @Input() isActiveMenu: boolean = false;
   @Output() isActiveMenuChange = new EventEmitter<boolean>();
+
   constructor(private scroller: ViewportScroller) {}
+
+  ngAfterViewInit(): void {}
 
   ngOnInit() {}
   closeMenu() {
