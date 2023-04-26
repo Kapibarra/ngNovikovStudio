72b132a4daaeb9fccb429fcac45c234ce47f8f8e
kapibarra2315 <nikita.nov2016@yandex.ru>
2023-04-26 16:17:53 +0300

    new services hero

--- src/app/main-page/services-page/shared/components/services-banner/services-banner.component.ts
+++ src/app/main-page/services-page/shared/components/services-banner/services-banner.component.ts
@@ -1,43 +1,73 @@
 import { Component, OnInit } from '@angular/core';
-import Swiper from 'swiper';
-
+import { ServiceBanner } from 'src/app/main-page/shared/models/servicesBanner';
+import SwiperCore, {
+  SwiperOptions,
+  Navigation,
+  Autoplay,
+  EffectFade,
+} from 'swiper';
+// install Swiper modules
+SwiperCore.use([Navigation, Autoplay, EffectFade]);
 @Component({
   selector: 'app-services-banner',
   templateUrl: './services-banner.component.html',
   styleUrls: ['./services-banner.component.scss'],
 })
 export class ServicesBannerComponent implements OnInit {
+  bannerSlider: ServiceBanner[] = [
+    {
+      title: ' VNUKOVO PREMIUM OUTLET ',
+      imgUrl: 'assets/images/portfolio/vnukovo.png',
+    },
+    {
+      title: ' montekrist media ',
+      imgUrl: 'assets/images/portfolio/montekrist.png',
+    },
+
+    {
+      title: ' Viper Official ',
+      imgUrl: 'assets/images/portfolio/viper.png',
+    },
+    {
+      title: ' fisio ',
+      imgUrl: 'assets/images/portfolio/fisio.png',
+    },
+    {
+      title: ' moscow startup ',
+      imgUrl: 'assets/images/portfolio/startup.png',
+    },
+    {
+      title: ' madstack ',
+      imgUrl: 'assets/images/portfolio/madstack.png',
+    },
+    {
+      title: ' kama matricia ',
+      imgUrl: 'assets/images/portfolio/kama.png',
+    },
+  ];
+
+  config: SwiperOptions = {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true,
+    },
+    navigation: false,
+    pagination: { clickable: true },
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      1920: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+    },
+  };
   constructor() {}
 
-  ngOnInit(): void {
-    const swiper = new Swiper('.swiper-container', {
-      direction: 'horizontal',
-      loop: true,
-      autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      breakpoints: {
-        // when window width is >= 1200px
-        1200: {
-          slidesPerView: 4,
-          spaceBetween: 30,
-        },
-        // when window width is >= 720px and < 1200px
-        720: {
-          slidesPerView: 3,
-          spaceBetween: 20,
-        },
-        // when window width is < 720px
-        0: {
-          slidesPerView: 1,
-          spaceBetween: 10,
-        },
-      },
-    });
-  }
+  ngOnInit(): void {}
 }
---
+++ src/app/main-page/shared/models/servicesBanner.ts
@@ -0,0 +1,8 @@
+export class ServiceBanner {
+  title: string;
+  imgUrl: string;
+  constructor(title: string, imgUrl: string) {
+    this.title = title;
+    this.imgUrl = imgUrl;
+  }
+}
