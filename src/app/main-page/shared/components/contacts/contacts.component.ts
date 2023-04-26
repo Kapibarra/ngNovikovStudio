075d540921747aba3335a1dce1eb3ae3cf07cbc6
kapibarra2315 <nikita.nov2016@yandex.ru>
2023-04-26 17:24:52 +0300

    new fixes for services page

--- src/app/main-page/shared/components/contacts/contacts.component.ts
+++ src/app/main-page/shared/components/contacts/contacts.component.ts
@@ -9,6 +9,9 @@ export class ContactsComponent implements OnInit {
   name: any;
   email: any;
   phone: any;
+  phoneNumber: string = '+7 967 059 62 39';
+  contactsEmail: string = 'info@novikovstudio.ru';
+  formattedPhoneNumber: string = this.phoneNumber.replace(/\s+/g, '');
   constructor() {}
 
   ngOnInit(): void {}
