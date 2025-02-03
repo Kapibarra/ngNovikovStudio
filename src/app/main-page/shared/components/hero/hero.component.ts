b9a36b0a1db823d023330c5aac128be657d40943
kapibarra2315 <nikita.nov2016@yandex.ru>
2025-02-04 00:32:57 +0300

    pewpewpew

--- src/app/main-page/shared/components/hero/hero.component.ts
+++ src/app/main-page/shared/components/hero/hero.component.ts
@@ -1,39 +1,28 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import SwiperCore, { SwiperOptions, Navigation, Autoplay } from 'swiper';
-// install Swiper modules
-SwiperCore.use([Navigation, Autoplay]);
+import {
+  Component,
+  ElementRef,
+  Renderer2,
+  AfterViewInit,
+  ViewChild,
+} from '@angular/core';
+
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.scss'],
 })
-export class HeroComponent implements OnInit {
+export class HeroComponent implements AfterViewInit {
+  @ViewChild('test-hero__item-xs', { static: false }) card:
+    | ElementRef
+    | undefined;
+
   listOfStrings = [
     'Web - Разработка',
     'Современный дизайн',
     'Digital Marketing',
   ];
-  config: SwiperOptions = {
-    slidesPerView: 2,
-    spaceBetween: 20,
-    loop: true,
-    navigation: false,
-    pagination: { clickable: true },
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    breakpoints: {
-      0: {
-        slidesPerView: 1, // 2 slides per view when screen width is 768px or greater
-      },
-      428: {
-        slidesPerView: 2, // 2 slides per view when screen width is 768px or greater
-      },
 
-      // You can add more breakpoints as needed
-    },
-  };
-  constructor() {}
-  ngOnInit(): void {}
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngAfterViewInit(): void {}
 }
